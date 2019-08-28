b22f681917a3cfb020399dd8a66ef476166cdcc3
Michael Eichenseer <16592938+mymindwentblvnk@users.noreply.github.com>
2019-08-28 14:57:20 +0200

    Simplify execution of notebook.

--- build.sh
+++
@@ -1,1 +0,0 @@
-docker build -t mymindwentblvnk/pythonback2basics .
--- run.sh
+++ run.sh
@@ -1,1 +1,2 @@
+docker build -t mymindwentblvnk/pythonback2basics .
 docker run -p 127.0.0.1:8888:8888 -v $(pwd)/notebooks:/notebooks mymindwentblvnk/pythonback2basics
