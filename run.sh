f062a7a83c6d6548afa19b32dac0fd0c577fcdc4
Michael Eichenseer <16592938+mymindwentblvnk@users.noreply.github.com>
2020-01-30 14:39:47 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
-docker build -t mymindwentblvnk/pythonback2basics .
-docker run -p 127.0.0.1:8888:8888 -v $(pwd)/notebooks:/notebooks mymindwentblvnk/pythonback2basics
+docker build -t mymindwentblvnk/jupyter-in-docker .
+docker run -p 127.0.0.1:8888:8888 -v $(pwd)/notebooks:/notebooks mymindwentblvnk/jupyter-in-docker
